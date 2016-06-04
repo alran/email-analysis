6fd69449045e2670f6d89067b2ed3b4855252e27
thomas-yancey <tomyncy618@gmail.com>
2016-06-04 19:04:02 -0400

    sent to user added

--- app/models/gmail.rb
+++ app/models/gmail.rb
@@ -6,28 +6,31 @@ include HTTParty
 
   attr_accessor :query
 
-  def initialize(query={q: "in:sent"}, user)
+  def initialize(query={q: "in:sent", "max_results": 20}, user)
     client = Signet::OAuth2::Client.new(access_token: user.google_oauth2.accesstoken)
     @service = Google::Apis::GmailV1::GmailService.new
     @service.authorization = client
+    @current_user_email = user.google_oauth2.email
     @query = query
   end
 
   def messages
-    res = @service.list_user_messages('me')
-    binding.pry
-    res
-
+    @service.list_user_messages('me')
   end
 
   def individual_messages_ids
     self.messages.messages.each do |message_thread|
-      self.individual_message_request(message_thread.id)
+      res = self.individual_message_request(message_thread.id)
+      sent_to = self.find_sent_to_user(res)
     end
   end
 
   def individual_message_request(id)
-    @service.get_user_message('me', id)
+    res = @service.get_user_message('me', id)
+  end
+
+  def find_sent_to_user(res)
+    res.payload.headers.each do {return val.value if val.name.match(/To/)}
   end
 
 end
