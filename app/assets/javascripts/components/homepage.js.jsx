109da5e049c2129c1aaa44915025dbf62775330f
Brian Bier <briansbier@gmail.com>
2016-06-04 15:45:56 -0400

    Create the homePage

---
+++ app/assets/javascripts/components/homepage.js.jsx
@@ -0,0 +1,18 @@
+var Homepage = React.createClass({
+  render: function(){
+    return (
+      <header className='bring-to-front'>
+        <div className="wrapper_wide">
+          <a href="#" id="logo">Nucleus</a>
+          <nav>
+          <h1 className="slogan">Sign up now to see an analysis of your emails.</h1>
+            <ul>
+              <li><a className='google-button' href="#">Google +</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+    );
+  }
+});
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604163808) do
+ActiveRecord::Schema.define(version: 20160604164222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "analyses", force: :cascade do |t|
+    t.string   "content_source", null: false
+    t.date     "date"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -54,18 +67,4 @@ ActiveRecord::Schema.define(version: 20160604163808) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "identities", "users"
-
-  create_table "analyses", force: :cascade do |t|
-    t.string   "content_source", null: false
-    t.date     "date"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  create_table "companies", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
