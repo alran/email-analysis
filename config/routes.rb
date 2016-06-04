26c48b9fff76cd67054488d25f24646e79d61432
FJ Collins Jr <fcollins@live.com>
2016-06-04 19:36:36 -0400

    add debuggin with pry methodologies

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :welcome, only: [:index]
+  resources :welcome
   resources :users, only: [:show, :edit, :update]
   root 'welcome#index'
 
