ad788be1157482a23f62133f0a3049bf9ee38c8b
Brian Bier <briansbier@gmail.com>
2016-06-04 17:05:26 -0400

    Working on the heatmap

---
+++ app/assets/javascripts/components/analysis.js.jsx
@@ -0,0 +1,21 @@
+var Analysis = React.createClass({
+  render: function(){
+    return (
+      <div class="row">
+        <div class="col-md-2">.col-md-6</div>
+        <div class="col-md-6">.col-md-6</div>
+      </div>
+      <ul id="test1">
+  <li>01</li>
+  <li>02</li>
+  <li>03</li>
+  <li>04</li>
+  <li>05</li>
+  <li>06</li>
+  <li>07</li>
+</ul>
+
+
+    );
+  }
+});
