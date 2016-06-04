a186a2dd3f3580c2082aba330f46a494f0ad565a
Alyssa <aransbury@gmail.com>
2016-06-04 17:00:11 -0400

    Add if/else statement to show different things if user is logged in or not

--- app/assets/javascripts/components/homepage.js.jsx
+++ app/assets/javascripts/components/homepage.js.jsx
@@ -1,18 +1,44 @@
 var Homepage = React.createClass({
+  userSignOut: function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: '/users/sign_out',
+      method: 'delete'
+    }).done(function(response){
+      window.location.href = '/'
+    });
+  },
   render: function(){
-    return (
-      <header className='bring-to-front'>
-        <div className="wrapper_wide">
-          <a href="#" id="logo">Nucleus</a>
-          <nav>
-          <h1 className="slogan">Sign up now to see an analysis of your emails.</h1>
-            <ul>
-              <li><a className='google-button' href="#">Google +</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-
-    );
+    if (this.props.loggedIn == 'false'){
+      return (
+        <header className='bring-to-front'>
+          <div className="wrapper_wide">
+            <a href="#" id="logo">Nucleus</a>
+            <nav>
+            <h1 className="slogan">Sign up now to see an analysis of your emails.</h1>
+              <ul>
+                <li><a className='google-button' href="/users/auth/google_oauth2">Google +</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+      );
+    } else {
+      var home = '/users/' + this.props.currentUser
+      return (
+        <header className='bring-to-front'>
+          <div className="wrapper_wide">
+            <a href="#" id="logo">Nucleus</a>
+            <nav>
+            <h1 className="slogan">Improve your relationships today</h1>
+              <ul>
+                <li><a className='google-button' href={home}>Home</a></li>
+                <li><a className='google-button' href="users/sign_out" onClick={this.userSignOut}>Logout</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+      );
+    }
   }
 });
