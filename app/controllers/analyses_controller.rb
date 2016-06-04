a850a6e1496ec469b565ba288b370532da6c517b
Alyssa <aransbury@gmail.com>
2016-06-04 12:54:46 -0400

    Add show and index routes for analyses

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,3 +1,11 @@
 class AnalysesController < ApplicationController
 
+  def show
+    # @analysis = Analysis.find_by()
+  end
+
+  def index
+    # @analyses = Analysis.where(user_id: current_user.id)
+  end
+
 end
