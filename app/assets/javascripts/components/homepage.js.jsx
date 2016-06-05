4873b8d078ab81cc7bb1bdf19688bd1e7f63c29d
Brian Bier <briansbier@gmail.com>
2016-06-05 02:34:14 -0400

    Style the user show page

--- app/assets/javascripts/components/homepage.js.jsx
+++ app/assets/javascripts/components/homepage.js.jsx
@@ -24,18 +24,19 @@ var Homepage = React.createClass({
     } else {
       var home = '/users/' + this.props.currentUser
       return (
-        <header className='bring-to-front'>
-          <div className="wrapper_wide">
-            <a href="#" id="logo">Nucleus</a>
-            <nav>
-            <h1 className="slogan">Improve your relationships today</h1>
-              <ul>
-                <li><a className='google-button' href={home}>Home</a></li>
-                <li><a className='google-button' href="users/sign_out" onClick={this.userSignOut}>Logout</a></li>
-              </ul>
-            </nav>
+        <div className="wrapper_skinny slogan">
+          <div className="center">
+          <a href="#" className="logo">Empath</a>
           </div>
-        </header>
+          <h1 className="h1">Improve your relationships today.</h1>
+          <div className="button_wrapper">
+            <a className='button button_highlight' href="/users/auth/google_oauth2">Google +</a>
+          </div>
+          <ul>
+            <li><a href={home}>Home</a></li>
+            <li><a href="users/sign_out" onClick={this.userSignOut}>Logout</a></li>
+          </ul>
+        </div>
       );
     }
   }
