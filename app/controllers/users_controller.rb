f6f9218236090e23092804b4673ed541ee37c71b
Alyssa <aransbury@gmail.com>
2016-06-04 22:33:15 -0400

    Convert instance variables to json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find_by(id: params[:id])
-    @analyses = Analysis.where(user_id: params[:id])
+    @user = User.find_by(id: params[:id]).to_json
+    @analyses = Analysis.where(user_id: params[:id]).to_json
   end
 
 end
