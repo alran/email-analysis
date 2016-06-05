cb008f9504940ef0e3f20b78948b18eee97aa02f
FJ Collins Jr <fcollins@live.com>
2016-06-05 00:23:12 -0400

    update email regex to filter content

--- app/models/gmail.rb
+++ app/models/gmail.rb
@@ -3,8 +3,10 @@ class Gmail
 include HTTParty
 
   base_uri "https://www.googleapis.com/gmail/v1"
-  REGEX = /.*(?=On [a-zA-Z]{3} \d{1}, 20[0-1][0-9], at ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9] [APap][mM])/
-  REGEX2 = /.*(?=On [a-zA-Z]{3,4}, [a-zA-Z]{3,4} \d{1}, 20[0-1][0-9], ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9] [APap][mM])/
+  REGEX = /.*(?=On [a-zA-Z]{3} \d{1,2}, 20[0-1][0-9], at ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9] [APap][mM])/
+  REGEX2 = /.*(?=On [a-zA-Z]{3,4}, [a-zA-Z]{3,4} \d{1,2}, 20[0-1][0-9], ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9] [APap][mM])/
+  REGEX3 = /.*(?=On [a-zA-Z]{3,4}, [a-zA-Z]{3,4} \d{1,2}, 20[0-1][0-9] at ([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9] [APap][mM])/
+  REGEX4 = /.*(?=<.*> wrote)/
 
   attr_accessor :query
 
@@ -30,9 +32,9 @@ include HTTParty
   end
 
   def find_email_content(res)
-    if res.payload.parts && res.payload.parts.first.body.data
+    if res.payload.parts.first.body.data
       string = res.payload.parts.first.body.data.gsub(/\s+/," ")
-      string.match(REGEX2||REGEX)
+      string.match(REGEX4||REGEX3||REGEX2||REGEX)
     elsif res.payload.body && res.payload.body.data
       string = res.payload.body.data.gsub(/\s+/," ")
     else
