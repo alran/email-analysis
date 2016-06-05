d09a0ceae129f8b5e2f1a0077c68aee5df146320
Alyssa <aransbury@gmail.com>
2016-06-04 23:12:43 -0400

    User profile has image, name, company, and role

--- app/assets/javascripts/components/userShow.js.jsx
+++ app/assets/javascripts/components/userShow.js.jsx
@@ -1,26 +1,19 @@
 var UserShow = React.createClass({
   render: function(){
+    user = JSON.parse(this.props.user)
+    company = JSON.parse(this.props.company)
+    analyses = JSON.parse(this.props.analyses)
+    image = JSON.parse(this.props.image)
     return (
       <div id="content" className="clearfix">
       <section id="left">
         <div id="userStats" className="clearfix">
           <div className="pic">
-            <a href="#"><img src="img/user_avatar.jpg" width="150" height="150" /></a>
+            <a href="#"><img src={image} width="150" height="150" /></a>
           </div>
           <div className="data">
-            <h1>Johnny Appleseed</h1>
-            <h3>San Francisco, CA</h3>
-            <h4><a href="http://spyrestudios.com/">http://spyrestudios.com/</a></h4>
-            <div className="socialMediaLinks">
-              <a href="http://twitter.com/jakerocheleau" rel="me" target="_blank"><img src="img/twitter.png" alt="@jakerocheleau" /></a>
-              <a href="http://gowalla.com/users/JakeRocheleau" rel="me" target="_blank"><img src="img/gowalla.png" /></a>
-            </div>
-            <div className="sep"></div>
-            <ul className="numbers clearfix">
-              <li>Reputation<strong>185</strong></li>
-              <li>Checkins<strong>344</strong></li>
-              <li className="nobrdr">Days Out<strong>127</strong></li>
-            </ul>
+            <h1>{user.name}</h1>
+            <h3>{user.category[0].toUpperCase() + user.category.slice(1)}, {company.name}</h3>
           </div>
         </div>
       </section>
