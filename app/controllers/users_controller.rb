00dc101f0e6cdd7e8b043bd5fc5e3a91829aad81
Alyssa <aransbury@gmail.com>
2016-06-04 23:13:03 -0400

    Pass image url and company name through to react user show component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find_by(id: params[:id]).to_json
+    user = User.find_by(id: params[:id])
+    @user = user.to_json
+    @image = user.google_oauth2.image.to_json
+    @company = Company.find_by(id: user.company_id).to_json
     @analyses = Analysis.where(user_id: params[:id]).to_json
   end
 
