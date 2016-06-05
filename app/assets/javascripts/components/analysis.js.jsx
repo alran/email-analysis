1ff1f9ce0427955ec634915c379f21f6e46f1579
Brian Bier <briansbier@gmail.com>
2016-06-04 21:17:33 -0400

    Create Watson Component

--- app/assets/javascripts/components/analysis.js.jsx
+++ app/assets/javascripts/components/analysis.js.jsx
@@ -37,4 +37,48 @@ var Analysis = React.createClass({
       </div>
     );
   }
+
+
+});
+var Watson = React.createClass({
+  render: function(){
+    return (
+      <div>
+      <h3 className="title">Email Analysis for Alyssa and Brian </h3>
+      <div className="table-cover">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Emotion</th>
+              <th>Behavior</th>
+              <th>Tone</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><a href="#">John 20%</a></td>
+              <td><a href="#">Doe 80%</a></td>
+            </tr>
+            <tr>
+              <td><a href="#">Jake 80%</a></td>
+              <td><a href="#">Doe 40%</a></td>
+            </tr>
+            <tr>
+              <td><a href="#">Chilly 10%</a></td>
+              <td><a href="#">billy 90%</a></td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+        <br/>
+        <br/>
+        <section>
+          <a href="#">Alyssa 80%</a><br/>
+          <a href="#">Analyze by email </a>
+        </section>
+
+      </div>
+    )
+  }
 });
