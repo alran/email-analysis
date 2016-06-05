2bb50fea02ad478466e98dd648e51bf2636cfa77
FJ Collins Jr <fcollins@live.com>
2016-06-05 00:59:07 -0400

    add emails show route and link to current user's email retrieval

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,7 +1,11 @@
 class EmailsController < ApplicationController
 
-  def get_emails #post route, redirects to analysis show page
+  def show #post route, redirects to analysis show page
     # post route to grab all emails from user and save in database
+    get_emails = Gmail.new(current_user)
+    get_emails.individual_messages_ids
+    @emails = Email.where(user_id: current_user.id)
+    redirect_to analysis_path
   end
 
 end
