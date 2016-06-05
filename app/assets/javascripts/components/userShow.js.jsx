57c15ef58540c6068f472b95dc1e8585a2338619
Brian Bier <briansbier@gmail.com>
2016-06-04 20:28:11 -0400

    Remove bootstrap component

--- app/assets/javascripts/components/userShow.js.jsx
+++ app/assets/javascripts/components/userShow.js.jsx
@@ -24,11 +24,6 @@ var UserShow = React.createClass({
           </div>
         </div>
       </section>
-      <BootstrapTable>
-           <TableHeaderColumn>Product ID</TableHeaderColumn>
-           <TableHeaderColumn >Product Name</TableHeaderColumn>
-           <TableHeaderColumn>Product Price</TableHeaderColumn>
-       </BootstrapTable>
     </div>
     );
   }
