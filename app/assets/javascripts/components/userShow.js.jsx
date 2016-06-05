c6631ad1f43765b606793b3fedf69dba9750cf47
Alyssa <aransbury@gmail.com>
2016-06-05 08:50:15 -0400

    Add links to previous analyses on user show page

--- app/assets/javascripts/components/userShow.js.jsx
+++ app/assets/javascripts/components/userShow.js.jsx
@@ -3,7 +3,14 @@ var UserShow = React.createClass({
     user = JSON.parse(this.props.user)
     company = JSON.parse(this.props.company)
     analyses = JSON.parse(this.props.analyses)
+    console.log(analyses)
     image = JSON.parse(this.props.image)
+    var analysisLinks = analyses.map(function(analysis){
+      var link = '/analyses/' + analysis.id
+      return (
+        <li><a href={link}>{analysis.created_at}</a></li>
+      )
+    })
     return (
     <div className="profile">
       <div className="wrapper_skinny">
@@ -18,6 +25,9 @@ var UserShow = React.createClass({
               <a className="button_message button_email" href="/emails/get_emails">Analyze Emails</a>
             </div>
           </li>
+        </ul>
+        <ul>
+          {analysisLinks}
         </ul>
       </div>
     </div>
