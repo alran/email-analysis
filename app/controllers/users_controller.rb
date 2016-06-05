73a38da486083c747abad70f5c25f50b24505626
Brian Bier <briansbier@gmail.com>
2016-06-05 03:23:38 -0400

    uncomment the users controller information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(id: params[:id])
-    # @user = user.to_json
-    # @image = user.google_oauth2.image.to_json
-    # @company = Company.find_by(id: user.company_id).to_json
-    # @analyses = Analysis.where(user_id: params[:id]).to_json
+    @user = user.to_json
+    @image = user.google_oauth2.image.to_json
+    @company = Company.find_by(id: user.company_id).to_json
+    @analyses = Analysis.where(user_id: params[:id]).to_json
   end
 
 end
